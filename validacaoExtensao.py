b6c4d1e6730ecb2e6dbd21e81fb3b2421a4ecb7d
Carlos <carloseduardoandrade@gmail.com>
2022-11-25 19:16:59 -0300

    Incluido :  - mais semelhante  - validação extensão  - hash percent

---
+++ validacaoExtensao.py
@@ -0,0 +1,19 @@
+#VALIDAÇÃO DA EXTENSÃO DO ARQUIVO DA IMAGEM A SER SALVA
+
+import pathlib
+from glob import glob
+import os
+
+
+i = 0
+img_names = glob(os.path.join("C:\\ImageBD", '*.*'))
+for fn in img_names:
+    file_extension = pathlib.Path(img_names[i]).suffix
+    print("Arquivo ", i + 1, "- ", img_names[i])
+    print("File Extension: ", file_extension)
+
+    if (file_extension == ".jpg") or (file_extension == ".jpeg"):
+        print("Arquivo válido!")
+    else:
+        print("Extensão inválida, por favor incluia arquivo com extensão .jpg")
+    i += 1
